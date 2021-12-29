2967211a6ef7e9837225c66a29abe8323f1b8d43
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2021-12-29 23:16:41 +0500

    adding table to output

--- modules/dynamodb/output.tf
+++ modules/dynamodb/output.tf
@@ -7,6 +7,15 @@ output "DEV_PORTAL_PRE_LOGIN_ACCOUNTS_TABLE_NAME" {
 output "DEV_PORTAL_FEEDBACK_TABLE_NAME" {
   value = aws_dynamodb_table.dynamo-dev-portal-feedback-db-table.id
 }
+//HERE
+output "API_PERMISSION_TABLE_NAME" {
+  value = aws_dynamodb_table.dynamo-api-permissions-db-table.id
+}
+
+output "MNO_THIRD_PARTY_RESOURCE_TABLE_NAME" {
+  value = aws_dynamodb_table.dynamo-mno-third-party-resource-db-table.id
+}
+
 
 output "DEV_PORTAL_CUSTOMERS_TABLE_ARN" {
   value = aws_dynamodb_table.dynamo-dev-portal-customer-db-table.arn
@@ -14,6 +23,21 @@ output "DEV_PORTAL_CUSTOMERS_TABLE_ARN" {
 output "DEV_PORTAL_PRE_LOGIN_ACCOUNTS_TABLE_ARN" {
   value = aws_dynamodb_table.dynamo-pre-login-accounts-db-table.arn
 }
+
+
+
+output "API_PERMISSION_TABLE_NAME" {
+  value = aws_dynamodb_table.dynamo-api-permissions-db-table.arn
+}
+
+output "MNO_THIRD_PARTY_RESOURCE_TABLE_ARN" {
+  value = aws_dynamodb_table.dynamo-mno-third-party-resource-db-table.arn
+}
+
+
+
+dynamo-mno-third-party-resource-db-table
+
 output "DEV_PORTAL_FEEDBACK_TABLE_ARN" {
   value = aws_dynamodb_table.dynamo-dev-portal-feedback-db-table.arn
 }
